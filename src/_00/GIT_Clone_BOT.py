f6f382e7cce6781636dad77f0be58e5172197f1c
Defendi <tiagodefendidasilva@gmail.com>
2024-05-27 13:19:36 -0300

    yaaa

--- src/_00/GIT_Clone_BOT.py
+++ src/_00/GIT_Clone_BOT.py
@@ -1,11 +1,15 @@
 import git
 import pandas as pd
+from datetime import datetime
 
 input_path = './src/_00/input/600_Starred_Projects_N.csv'
 input_file = pd.read_csv(input_path)
 output_path = './src/_00/output'
 
+start = datetime.now()
+
 for repository, language in zip(input_file['url'], input_file['main language']):
+    start_i = datetime.now()
     local_repo_directory = f"{output_path}/{language}/{repository.split('/')[-2]}~{repository.split('/')[-1]}"
     print(f"{repository.split('/')[-2]}~{repository.split('/')[-1]}", end="")
 
@@ -26,5 +30,14 @@ for repository, language in zip(input_file['url'], input_file['main language']):
             print("\033[31mDestination path already exists and is not an empty directory\033[m")
         else:
             print(f"\033[31mAn error occurred: {e}\033[m")
+
+    end_i = datetime.now()
+    time = pd.DataFrame({'start' : [start_i],'end':[end_i],'time_expended':[end_i-start_i]})
+    time.to_csv(f'{output_path}/{language}/{repository.split('/')[-2]}~{repository.split('/')[-1]}~time.csv')
+
+end = datetime.now()
+
+time = pd.DataFrame({'start' : start, 'end':end, 'time_expended':[end-start]})
+time.to_csv(f'{output_path}/time~total.csv')
 
 print('DONE!')
