ab7acb62afac1197a90c7a8a790da6f15de0eefe
Defendi <tiagodefendidasilva@gmail.com>
2024-06-23 12:01:07 -0300

    🥶
    RECEBA!

--- src/_00/GIT_Clone_BOT.py
+++ src/_00/GIT_Clone_BOT.py
@@ -2,7 +2,7 @@ import git
 import pandas as pd
 from datetime import datetime
 
-input_path = './src/_00/input/600_Starred_Projects_N.csv'
+input_path = './src/_00/input/600_Starred_Projects_N copy.csv'
 input_file = pd.read_csv(input_path)
 output_path = './src/_00/output'
 
@@ -21,7 +21,15 @@ for repository, language in zip(input_file['url'], input_file['main language']):
                 repo = git.Repo.clone_from(repository, local_repo_directory, branch='master')
                 print(" -> Master branch")
             except git.exc.GitCommandError:
-                print(" -> \033[31mNo Main/Master branches found\033[m")
+                try:
+                    repo = git.Repo.clone_from(repository, local_repo_directory, branch='2023.x')
+                    print(" -> 2023.x")
+                except git.exc.GitCommandError:
+                    try:
+                        repo = git.Repo.clone_from(repository, local_repo_directory, branch='3.2')
+                        print(" -> 3.2")
+                    except git.exc.GitCommandError:
+                        print(" -> \033[31mNo Main/Master branches found\033[m")
 
     except git.exc.GitCommandError as e:
         print()
