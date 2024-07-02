1f49ef9761e002caec68172430cc8a35f418de81
Matt Batichotti <52006006+MateusCohuzer@users.noreply.github.com>
2024-07-01 22:16:34 -0300

    🎉

--- teste-pickle_binary_save.py
+++ teste-pickle_binary_save.py
@@ -7,3 +7,12 @@ for commit in Repository('https://github.com/iptv-org/iptv').traverse_commits():
     with open(f'commit_{i}', 'ab') as arquivo_binario:
       pickle.dump(commit, arquivo_binario)
     i += 1
+
+
+with open("commit_1", "rb") as arquivo_binario:
+  while True:
+    try:
+        obj = pickle.load(arquivo_binario)
+        print(obj.author.name)
+    except EOFError:
+        break
