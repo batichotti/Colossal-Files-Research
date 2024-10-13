cd18f77a2cf28c5172110d092b764f71b5286f8c
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-13 13:53:48 -0300

    Now with threads 🚀

--- src/_04/drill.py
+++ src/_04/drill.py
@@ -2,7 +2,7 @@ import pydriller as dr
 import pandas as pd
 import os
 from datetime import datetime
-import sys
+from concurrent.futures import ThreadPoolExecutor
 
 # setting paths -------------------------------------------------------------------------------------------------------
 
@@ -14,9 +14,9 @@ repositories_list_path: str = './src/_00/input/600_Starred_Projects.csv'
 
 # base dirs
 repositories_base_dir: str = './src/_00/output/'
-files_base_path:str = './src/_03/output/'
+files_base_path: str = './src/_03/output/'
 
-#date
+# Date
 date = datetime(2024, 10, 8, 17, 0, 0)
 
 # preparing environment -----------------------------------------------------------------------------------------------
@@ -24,37 +24,36 @@ date = datetime(2024, 10, 8, 17, 0, 0)
 # loading list of repositories
 repositories: pd.DataFrame = pd.read_csv(repositories_list_path, engine='python')
 
-for i in range(len(repositories)):
-    # start timer
+# function to process each repository
+def process_repository(i):
+    # Start timer
     start = datetime.now()
 
-# ---------------------------------------------------------------------------------------------------------------------
-
-    # getting repository information
-    main_language:str = repositories['main language'].loc[i]
-    owner:str = repositories['owner'].loc[i]
-    project:str = repositories['project'].loc[i]
+    # Getting repository information
+    main_language: str = repositories['main language'].loc[i]
+    owner: str = repositories['owner'].loc[i]
+    project: str = repositories['project'].loc[i]
     branch: str = repositories['branch'].loc[i]
 
-    # generating repository path
+    # Generating repository path
     repository_path: str = f'{repositories_base_dir}{main_language}/{owner}~{project}'
     print(f'{repository_path} -> {branch}')
 
-    # generating the path to the repository's files list
+    # Generating the path to the repository's files list
     files_list_path: str = f'{files_base_path}{main_language}/{owner}~{project}.csv'
     print(f'    > {files_list_path}')
 
-    # loading files list
+    # Loading files list
     files_list: pd.DataFrame = pd.read_csv(files_list_path, sep='|', engine='python')
 
     for j in range(len(files_list)):
-        # for each file generating a path
-        file_path: str = files_list['path'].loc[j] # ./src/_00/output/Java/batichotti~Clube-Do-Filme/web/buscar.jsp
-        file_name: str = file_path.split('/')[-1] # buscar.jsp
-        file_path = '/'.join(file_path.split('/')[6:]) # web/buscar.jsp
+        # For each file, generate a path
+        file_path: str = files_list['path'].loc[j]
+        file_name: str = file_path.split('/')[-1]
+        file_path = '/'.join(file_path.split('/')[6:])
         print(f'        > {file_name}:{file_path}')
 
-# pydriller -----------------------------------------------------------------------------------------------------------
+        # PyDriller  -----------------------------------------------------------------------------------------------
 
         dir_path = f'{output_path}{main_language}/{owner}~{project}'
         os.makedirs(dir_path, exist_ok=True)
@@ -63,13 +62,13 @@ for i in range(len(repositories)):
 
         for commit in repository.traverse_commits():
             try:
-                # setting commit path
+                # Setting commit path
                 commit_dir = f'{dir_path}/{commit.hash}'
                 if os.path.exists(commit_dir):
                     continue
                 os.makedirs(commit_dir, exist_ok=True)
 
-                # analyzing and saving commit information
+                # Analyzing and saving commit information
                 df_commit: pd.DataFrame = pd.DataFrame({
                     'Hash': [commit.hash],
                     'Project Name': [commit.project_name],
@@ -88,12 +87,12 @@ for i in range(len(repositories)):
                 })
                 df_commit.to_csv(f'{commit_dir}/commit.csv', sep='|')
 
-                # setting file path
+                # Setting file path
                 files_dir = f'{commit_dir}/files/'
                 os.makedirs(files_dir, exist_ok=True)
 
                 for file in commit.modified_files:
-                    # analyzing and saving each commit's file information
+                    # Analyzing and saving each commit's file information
                     df_file: pd.DataFrame = pd.DataFrame({
                         'File Name': [file.filename],
                         'Change Type': [str(file.change_type).split('.')[-1]],
@@ -111,22 +110,23 @@ for i in range(len(repositories)):
             except Exception as e:
                 print(f'Error: {e}')
 
-                # error dir
+                # Error dir
                 error_dir = './mined_commits/errors/'
                 os.makedirs(error_dir, exist_ok=True)
 
-                # saving errors
+                # Saving errors
                 df_commit.to_csv(f'{error_dir}errors_{commit.project_name}.csv', mode='a', sep='|', header=False)
 
-# ---------------------------------------------------------------------------------------------------------------------
-
-    # end timer and saving it to csv
+    # End timer and save it to csv
     end = datetime.now()
-
     total_time = pd.DataFrame({
-        'Star' : [start],
-        'End' : [end],
-        'TOTAL' : [end-start],
+        'Start': [start],
+        'End': [end],
+        'TOTAL': [end - start],
     })
     total_time.to_csv(f'{output_path}{main_language}/{owner}~{project}/total_time.csv', index=False)
-    print(end-start)
+    print(end - start)
+
+if __name__ == '__main__':
+    with ThreadPoolExecutor(max_workers=4) as executor: #Adjust looking at the CPU
+        executor.map(process_repository, range(len(repositories)))
