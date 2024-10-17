9548926ea7291f6b120f426451f33b5168bc09f3
Matt Batichotti <52006006+batichotti@users.noreply.github.com>
2024-10-17 16:28:41 -0300

    threads

--- src/_00/GIT_Clone_BOT.py
+++ src/_00/GIT_Clone_BOT.py
@@ -1,34 +1,40 @@
 import git
 import pandas as pd
 from datetime import datetime
+from os import cpu_count
+from concurrent.futures import ThreadPoolExecutor
 
 input_path = './src/_00/input/600_Starred_Projects_vLite.csv'
 output_path = './src/_00/output'
-
 input_file = pd.read_csv(input_path)
 
 start = datetime.now()
+max_workers = cpu_count()
 
-for repository, language, branch in zip(input_file['url'], input_file['main language'], input_file['branch']):
+def clone_repository(repository, language, branch):
     local_repo_directory = f"{output_path}/{language}/{repository.split('/')[-2]}~{repository.split('/')[-1]}"
     print(f"{repository.split('/')[-2]}~{repository.split('/')[-1]}", end="")
 
     try:
         try:
-            repo = git.Repo.clone_from(repository, local_repo_directory, branch=branch)
+            git.Repo.clone_from(repository, local_repo_directory, branch=branch)
             print(f" -> {branch} branch")
         except git.exc.GitCommandError:
             print(" -> \033[31mNo Default branches found\033[m")
-
     except git.exc.GitCommandError as e:
         print()
-        if "already exists and is not an empty directory" in e.stderr:
+        if "already exists and is not an empty directory" in str(e):
             print("\033[31mDestination path already exists and is not an empty directory\033[m")
         else:
             print(f"\033[31mAn error occurred: {e}\033[m")
 
+# Using ThreadPoolExecutor to parallelize the cloning process
+with ThreadPoolExecutor(max_workers=max_workers) as executor:
+    for repository, language, branch in zip(input_file['url'], input_file['main language'], input_file['branch']):
+        executor.submit(clone_repository, repository, language, branch)
+
 end = datetime.now()
-time = pd.DataFrame({'start' : start, 'end':end, 'time_expended':[end-start]})
-time.to_csv(f'{output_path}/time~total.csv', index=False)
+time_df = pd.DataFrame({'start': [start], 'end': [end], 'time_expended': [end - start]})
+time_df.to_csv(f'{output_path}/time~total.csv', index=False)
 
 print('DONE!')
--- src/_01/CLoC_BOT.py
+++ src/_01/CLoC_BOT.py
@@ -1,58 +1,61 @@
-from os import path, listdir, system, remove
+from os import path, listdir, system, remove, cpu_count
 from platform import system as op_sys
 import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
+
 SEPARATOR = '|'
 
-def formater(file_path:str, separator:str=','):
+def formater(file_path: str, separator: str = ','):
     try:
         file = pd.read_csv(file_path, sep=separator, low_memory=False)
-
         try:
             columns_to_remove = [col for col in file.columns if col.startswith("github.com/AlDanial/cloc")]
-            file = file.drop(columns=columns_to_remove) # removing watermark
+            file = file.drop(columns=columns_to_remove)  # removing watermark
 
             sum_index = file.index[file.iloc[:, 0].str.startswith("SUM")]
-            file = file.loc[:sum_index[0] - 1] if sum_index.any() else file # removing lines that we are not interested in
+            file = file.loc[:sum_index[0] - 1] if sum_index.any() else file  # removing lines that we are not interested in
 
-            file = file.rename(columns={'filename':'path'})
+            file = file.rename(columns={'filename': 'path'})
 
             file['owner'] = file['path'].apply(lambda x: x.split('\\')[5].split('~')[0])
             file['project'] = file['path'].apply(lambda x: x.split('\\')[5].split('~')[1])
-            file['file'] = file['path'].apply(lambda x: x.split('\\')[-1]) # adding columns to the owner, project, and file name
+            file['file'] = file['path'].apply(lambda x: x.split('\\')[-1])  # adding columns to the owner, project, and file name
 
-            file = file[['path', 'owner', 'project', 'file', 'language', 'code', 'comment', 'blank']] # rearranging csv
+            file = file[['path', 'owner', 'project', 'file', 'language', 'code', 'comment', 'blank']]  # rearranging csv
 
             file.to_csv(file_path, sep=separator, index=False)
-
-        except:
-            print('ERROR#???')
-    
-    except:
+        except Exception as e:
+            print(f'Error during formatting: {e}')
+    except Exception as e:
         remove(file_path)
-        print(f'\033[31mSeparator fucked up, reestract with Windows(\033[35m{file_path}.csv\033[31m)\033[m')
+        print(f'\033[31mSeparator error, re-extract with Windows(\033[35m{file_path}.csv\033[31m)\033[m Error: {e}')
+
+def process_repository(cloc_path, input_path, output_path, language, repository):
+    repo_input_path = f'{input_path}/{language}/{repository}'
+    repo_output_path = f'{output_path}/{language}/{repository}.csv'
+
+    if path.exists(repo_output_path):
+        print(f"\033[31mDestination path (\033[35m{repository}.csv\033[31m) already exists and is not an empty directory\033[m")
+    else:
+        if op_sys() == "Windows":
+            system(f'{cloc_path} --by-file-by-lang --csv-delimiter="{SEPARATOR}" --out {repo_output_path} {repo_input_path}')  # run CLoC
+        else:
+            system(f'{cloc_path} --by-file-by-lang --csv --out={repo_output_path} {repo_input_path}')  # for Linux
+
+        formater(repo_output_path, SEPARATOR)
+        
+        if path.exists(repo_output_path):
+            print(f'\n File \033[35m{repository}.csv\033[m was created successfully \n')
 
 input_path = './src/_00/output'
 output_path = './src/_01/output'
+cloc_path = path.abspath("./src/_01/input/cloc.exe") if op_sys() == "Windows" else 'cloc'
+max_workers = cpu_count()
 
-if op_sys() == "Windows":
-    cloc = path.abspath("./src/_01/input/cloc.exe")  # CLoC.exe path
-else:
-    cloc = 'cloc' # REMEMBER TO INSTALL CLOC
-
-# running CLoC for each cloned repositories
-for language in listdir(input_path):
-    if language != f'time~total.csv':
-        language_path = path.join(input_path, language)
-        for repository in listdir(language_path):
-            if path.exists(f'{output_path}/{language}/{repository}.csv'):
-                print(f"\033[31mDestination path (\033[35m{repository}.csv\033[31m) already exists and is not an empty directoryn\033[m")
-            else:
-                if op_sys() == "Windows":
-                    system(f'{cloc} --by-file-by-lang --csv-delimiter="{SEPARATOR}" --out {output_path}/{language}/{repository}.csv {input_path}/{language}/{repository}') # runing CLoC
-                    formater(f'{output_path}/{language}/{repository}.csv', SEPARATOR)
-                else:               
-                    system(f'{cloc} --by-file-by-lang --csv --out={output_path}/{language}/{repository}.csv {input_path}/{language}/{repository}') # for Linux
-                    formater(f'{output_path}/{language}/{repository}.csv')
-                
-                if path.exists(f'{output_path}/{language}/{repository}.csv'):
-                    print(f'\n File \033[35m{repository}.csv\033[m was created successfully \n')
+# Using ThreadPoolExecutor to parallelize the process
+with ThreadPoolExecutor(max_workers=max_workers) as executor:
+    for language in listdir(input_path):
+        if language != 'time~total.csv':
+            language_path = path.join(input_path, language)
+            for repository in listdir(language_path):
+                executor.submit(process_repository, cloc_path, input_path, output_path, language, repository)
