0aa71d35e3630796a3ba965fb9a53f6b4e516577
Defendi <tiagodefendidasilva@gmail.com>
2024-10-20 16:44:36 -0300

    Create Clone_CLoC.py

---
+++ src/_00.1/Clone_CLoC.py
@@ -0,0 +1,128 @@
+import git
+import pandas as pd
+from datetime import datetime
+from os import path, listdir, system, remove
+from platform import system as op_sys
+SEPARATOR = '|'
+
+#-------------------------------------------------------------------------------------------------
+
+def formater(file_path:str, separator:str=','):
+    try:
+        file = pd.read_csv(file_path, sep=separator, low_memory=False)
+
+        try:
+            columns_to_remove = [col for col in file.columns if col.startswith("github.com/AlDanial/cloc")]
+            file = file.drop(columns=columns_to_remove) # removing watermark
+
+            sum_index = file.index[file.iloc[:, 0].str.startswith("SUM")]
+            file = file.loc[:sum_index[0] - 1] if sum_index.any() else file # removing lines that we are not interested in
+
+            file = file.rename(columns={'filename':'path'})
+
+            file['owner'] = file['path'].apply(lambda x: x.split('\\')[5].split('~')[0])
+            file['project'] = file['path'].apply(lambda x: x.split('\\')[5].split('~')[1])
+            file['file'] = file['path'].apply(lambda x: x.split('\\')[-1]) # adding columns to the owner, project, and file name
+
+            file = file[['path', 'owner', 'project', 'file', 'language', 'code', 'comment', 'blank']] # rearranging csv
+
+            file.to_csv(file_path, sep=separator, index=False)
+
+        except:
+            print('ERROR#???')
+            input('primeiro')
+            remove(file_path)
+
+    except:
+        print(f'\033[31mSeparator error, reprocess with Windows(\033[35m{file_path}.csv\033[31m)\033[m')
+        input('segundo')
+        remove(file_path)
+
+
+#-------------------------------------------------------------------------------------------------
+
+#SETUP
+input0_path = './src/_00/input/600_Starred_Projects_vLite.csv'
+output0_path = './src/_00.1/output-0-clone'
+
+input_file = pd.read_csv(input0_path)
+
+input1_path = './src/_00.1/output-0-clone'
+output1_path = './src/_00.1/output-1-cloc'
+
+if op_sys() == "Windows":
+    cloc = path.abspath("./src/_01/input/cloc.exe")  # CLoC.exe path
+else:
+    cloc = 'cloc'  # REMEMBER TO INSTALL CLOC
+
+start = datetime.now()
+
+#-------------------------------------------------------------------------------------------------
+
+index = 0
+while index < len(input_file):
+    repository, language, branch = input_file.loc[index, ['url', 'main language', 'branch']]
+    repo_path = f"{language}/{repository.split('/')[-2]}~{repository.split('/')[-1]}"
+    local_repo_directory = f"{output0_path}/{repo_path}"
+    print(f"{repository.split('/')[-2]}~{repository.split('/')[-1]}", end="")
+
+    try:
+        try:
+            repo = git.Repo.clone_from(repository, local_repo_directory, branch=branch)
+            print(f" -> {branch} branch")
+        except git.exc.GitCommandError:
+            print(" -> \033[31mNo Default branches found\033[m")
+
+    except git.exc.GitCommandError as e:
+        print()
+        if "already exists and is not an empty directory" in e.stderr:
+            print("\033[31mDestination path already exists and is not an empty directory\033[m")
+        else:
+            print(f"\033[31mAn error occurred: {e}\033[m")
+
+#-------------------------------------------------------------------------------------------------
+
+    try:
+        cloc_repo_path = f"{output1_path}/{repo_path}"
+        if path.exists(f'{cloc_repo_path}.csv'):
+            print(f"\033[31mDestination path (\033[35m{repository}.csv\033[31m) already exists and is not an empty directory\n\033[m")
+        else:
+            if op_sys() == "Windows":
+                system(f'{cloc} --by-file-by-lang --csv-delimiter="{SEPARATOR}" --out {cloc_repo_path}.csv {local_repo_directory}')  # running CLoC
+                formater(f'{cloc_repo_path}.csv', SEPARATOR)
+            else:
+                system(f'{cloc} --by-file-by-lang --csv --out={cloc_repo_path}.csv {local_repo_directory}')  # for Linux
+                formater(f'{cloc_repo_path}.csv')
+
+            if path.exists(f'{cloc_repo_path}.csv'):
+                print(f'\n File \033[35m{repository}.csv\033[m was created successfully \n')
+    except:
+        print("Erro")
+
+#-------------------------------------------------------------------------------------------------
+
+    try:
+        cloc_df = pd.read_csv(f'{cloc_repo_path}.csv', sep=SEPARATOR, low_memory=False)
+
+        for file in cloc_df['path']:
+            if not path.exists(file):
+                if path.exists(local_repo_directory):
+                    remove(local_repo_directory)
+                if path.exists(cloc_repo_path):
+                    remove(cloc_repo_path)
+                print("Error1")
+                break
+    except:
+        print("Erro2")
+        continue  # Reinicia o laço sem avançar para o próximo repositório
+
+    # Avança para o próximo repositório se o código rodou sem erros
+    index += 1
+
+#-------------------------------------------------------------------------------------------------
+
+end = datetime.now()
+time = pd.DataFrame({'start': start, 'end': end, 'time_expended': [end - start]})
+time.to_csv(f'{output0_path}/time~total.csv', index=False)
+
+print('DONE!')
