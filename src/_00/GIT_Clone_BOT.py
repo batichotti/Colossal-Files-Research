47ae1abd067e453505935077aaaee26f690436ae
Defendi <tiagodefendidasilva@gmail.com>
2024-11-23 11:23:12 -0300

    git clone

--- src/_00/GIT_Clone_BOT.py
+++ src/_00/GIT_Clone_BOT.py
@@ -1,7 +1,7 @@
 import git
 import pandas as pd
 from datetime import datetime
-from os import cpu_count
+from os import cpu_count, path
 from concurrent.futures import ThreadPoolExecutor
 
 input_path = './src/_00/input/600_Starred_Projects_vLite.csv'
@@ -12,21 +12,26 @@ start = datetime.now()
 max_workers = cpu_count()
 
 def clone_repository(repository, language, branch):
-    local_repo_directory = f"{output_path}/{language}/{repository.split('/')[-2]}~{repository.split('/')[-1]}"
-    print(f"{repository.split('/')[-2]}~{repository.split('/')[-1]}", end="")
+    repo_path = f"{language}/{repository.split('/')[-2]}~{repository.split('/')[-1]}"
+    local_repo_directory = f"{output_clone_path}/{repo_path}"
+    print(f"{repository.split('/')[-2]}~{repository.split('/')[-1]}", end='')
 
-    try:
+    if not path.exists(local_repo_directory):
         try:
-            git.Repo.clone_from(repository, local_repo_directory, branch=branch)
-            print(f" -> {branch} branch")
-        except git.exc.GitCommandError:
-            print(" -> \033[31mNo Default branches found\033[m")
-    except git.exc.GitCommandError as e:
-        print()
-        if "already exists and is not an empty directory" in str(e):
-            print("\033[31mDestination path already exists and is not an empty directory\033[m")
-        else:
-            print(f"\033[31mAn error occurred: {e}\033[m")
+            try:
+                print(f" - Cloning...")
+                repo = git.Repo.clone_from(repository, local_repo_directory, branch=branch)
+                print(f"Cloned -> {branch} branch")
+            except git.exc.GitCommandError:
+                print(f" > \033[31mNo Default branches found\033[m - {branch}\n{repository}")
+                input()
+        except git.exc.GitCommandError as e:
+            print()
+            if "already exists and is not an empty directory" in e.stderr:
+                print("\033[31mDestination path already exists and is not an empty directory\033[m")
+            else:
+                print(f"\033[31mAn error occurred in Clone:\n{e}\033[m")
+    print()
 
 with ThreadPoolExecutor(max_workers=max_workers) as executor:
     for repository, language, branch in zip(input_file['url'], input_file['main language'], input_file['branch']):
