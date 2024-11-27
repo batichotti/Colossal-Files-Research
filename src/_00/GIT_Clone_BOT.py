34d1bb16b70a64a2da5463a958b1a2efcfb5e76e
Defendi <tiagodefendidasilva@gmail.com>
2024-11-26 22:06:31 -0300

    clone sem threads para teste

--- src/_00/GIT_Clone_BOT.py
+++ src/_00/GIT_Clone_BOT.py
@@ -1,17 +1,23 @@
 import git
 import pandas as pd
 from datetime import datetime
-from os import cpu_count, path
-from concurrent.futures import ThreadPoolExecutor
+from os import path
+from platform import system as op_sys
 
-input_path = './src/_00/input/600_Starred_Projects_vLite.csv'
+# SETUP ================================================================================================================
+input_path = './src/_00/input/600_Starred_Projects.csv'
 output_path = './src/_00/output'
+
 input_file = pd.read_csv(input_path)
 
 start = datetime.now()
-max_workers = cpu_count()
 
-def clone_repository(repository, language, branch):
+# Code =================================================================================================================
+index = 0
+while index < len(input_file):
+# Clone ================================================================================================================
+    repository, language, branch = input_file.loc[index, ['url', 'main language', 'branch']]
+
     repo_path = f"{language}/{repository.split('/')[-2]}~{repository.split('/')[-1]}"
     local_repo_directory = f"{output_path}/{repo_path}"
     print(f"{repository.split('/')[-2]}~{repository.split('/')[-1]}", end='')
@@ -24,7 +30,6 @@ def clone_repository(repository, language, branch):
                 print(f"Cloned -> {branch} branch")
             except git.exc.GitCommandError:
                 print(f" > \033[31mNo Default branches found\033[m - {branch}\n{repository}")
-                input()
         except git.exc.GitCommandError as e:
             print()
             if "already exists and is not an empty directory" in e.stderr:
@@ -33,12 +38,11 @@ def clone_repository(repository, language, branch):
                 print(f"\033[31mAn error occurred in Clone:\n{e}\033[m")
     print()
 
-with ThreadPoolExecutor(max_workers=max_workers) as executor:
-    for repository, language, branch in zip(input_file['url'], input_file['main language'], input_file['branch']):
-        executor.submit(clone_repository, repository, language, branch)
+    index += 1
 
+#=======================================================================================================================
 end = datetime.now()
-time_df = pd.DataFrame({'start': [start], 'end': [end], 'time_expended': [end - start]})
-time_df.to_csv(f'{output_path}/time~total.csv', index=False)
+time = pd.DataFrame({'start': start, 'end': end, 'time_expended': [end - start]})
+time.to_csv(f'{output_path}/time~total.csv', index=False)
 
 print('DONE!')
