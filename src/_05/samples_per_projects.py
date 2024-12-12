39a77b5cbef2b7cd682c5f80a19903297a8a58bd
Defendi <tiagodefendidasilva@gmail.com>
2024-12-11 22:31:45 -0300

    x_x

--- src/_05/samples_per_projects.py
+++ src/_05/samples_per_projects.py
@@ -15,12 +15,17 @@ SEPARATOR = '|'
 input_path:str = "./src/_05/input/"
 output_path = "./src/_05/output/"
 
-repositories_path:str = "./src/_00/input/600_Starred_Projects.csv"
+repositories_path:str = "./src/_00/input/linux.csv"
 cloc_path:str = "./src/_01/output/"
 large_files_path:str = "./src/_03/output/"
+sample_path:str = f"{input_path}files_sample.csv"
 
 repositories:pd.DataFrame = pd.read_csv(repositories_path)
 # print(repositories)
+
+sample_df:pd.DataFrame = pd.read_csv(sample_path)
+# sample_df = sample_df.drop(columns=["all", "larges", "small", "2%", "3%", "4%", "5%"]).rename(columns={"1%":"small total"})
+# print(sample_df)
 
 # ======================================================================================================================
 
@@ -34,16 +39,18 @@ def main()->None:
         print(repo_path)
 
         repository_files_df:pd.DataFrame = pd.read_csv(f"{cloc_path}/{repo_path}.csv", sep=SEPARATOR)
-        repository_files_df = repository_files_df.groupby('language')
-        repository_files_df = repository_files_df.size()
+        repository_files_df = repository_files_df.groupby('language').size()
         # print(repository_files_df)
 
         large_files_df:pd.DataFrame = pd.read_csv(f"{large_files_path}/{repo_path}.csv", sep=SEPARATOR)
-        large_files_df = large_files_df.groupby('language')
-        large_files_df = large_files_df.size()
+        large_files_df = large_files_df.groupby('language').size()
         # print(large_files_df)
 
-        
+        merged_df = pd.concat([repository_files_df, large_files_df], axis=1).dropna()
+        merged_df = merged_df.rename(columns={0: 'total'}).rename(columns={1: 'large files'})
+        merged_df['small proportion'] = merged_df['large files'] / merged_df['total']
+        merged_df['small files'] = merged_df['total'] * merged_df['small proportion']
+        print(merged_df)
 
 
 
