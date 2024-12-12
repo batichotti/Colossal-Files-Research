a5bf0547f12464aadf1e48f2f72c49f1733769e1
Defendi <tiagodefendidasilva@gmail.com>
2024-12-11 23:23:20 -0300

    done porra

--- src/_05/samples_per_projects.py
+++ src/_05/samples_per_projects.py
@@ -7,6 +7,8 @@
 '''
 
 import pandas as pd
+from math import ceil
+from os import makedirs
 
 SEPARATOR = '|'
 
@@ -15,7 +17,7 @@ SEPARATOR = '|'
 input_path:str = "./src/_05/input/"
 output_path = "./src/_05/output/"
 
-repositories_path:str = "./src/_00/input/linux.csv"
+repositories_path:str = "./src/_00/input/600_Starred_Projects.csv"
 cloc_path:str = "./src/_01/output/"
 large_files_path:str = "./src/_03/output/"
 sample_path:str = f"{input_path}files_sample.csv"
@@ -24,8 +26,7 @@ repositories:pd.DataFrame = pd.read_csv(repositories_path)
 # print(repositories)
 
 sample_df:pd.DataFrame = pd.read_csv(sample_path)
-# sample_df = sample_df.drop(columns=["all", "larges", "small", "2%", "3%", "4%", "5%"]).rename(columns={"1%":"small total"})
-# print(sample_df)
+sample_df:pd.Series = sample_df.set_index('language')['1%']
 
 # ======================================================================================================================
 
@@ -35,6 +36,7 @@ def main()->None:
         repository, language = repositories.loc[i, ['url', 'main language']]
         # print(f"rep: {repository}, lang: {language}")
 
+        makedirs(f"{output_path}{language}", exist_ok=True)
         repo_path = f"{language}/{repository.split('/')[-2]}~{repository.split('/')[-1]}"
         print(repo_path)
 
@@ -46,12 +48,15 @@ def main()->None:
         large_files_df = large_files_df.groupby('language').size()
         # print(large_files_df)
 
-        merged_df = pd.concat([repository_files_df, large_files_df], axis=1).dropna()
-        merged_df = merged_df.rename(columns={0: 'total'}).rename(columns={1: 'large files'})
+        merged_df = pd.concat([repository_files_df, large_files_df, sample_df], axis=1).dropna()
+        merged_df = merged_df.rename(columns={0: 'total'}).rename(columns={1: 'large files'}).rename(columns={'1%': 'small p/ language'})
         merged_df['small proportion'] = merged_df['large files'] / merged_df['total']
-        merged_df['small files'] = merged_df['total'] * merged_df['small proportion']
-        print(merged_df)
+        merged_df['small files'] = merged_df['small p/ language'] * merged_df['small proportion']
+        merged_df['small files'] = merged_df['small files'].apply(ceil)
+        merged_df['files available'] = merged_df['total'] - merged_df['large files']
+        # print(merged_df)
 
+        merged_df.to_csv(f"{output_path}{repo_path}.csv")
 
 
 if (__name__ == "__main__"):
