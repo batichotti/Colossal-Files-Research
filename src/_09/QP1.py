bee8142541cbc094c88430288f2bd2389f12504f
Defendi <tiagodefendidasilva@gmail.com>
2025-03-10 13:26:47 -0300

    comecando o script

--- src/_09/QP1.py
+++ src/_09/QP1.py
@@ -13,3 +13,18 @@ large_files_path:str = "./src/_03/output/"
 
 repositories:pd.DataFrame = pd.read_csv(repositories_path)
 
+# script
+
+for i in range(len(repositories)):
+    # counting files
+    
+    # getting repository information
+    repository, language = repositories.loc[i, ['url', 'main language']]
+
+    makedirs(f"{output_path}{language}", exist_ok=True)
+    repo_path = f"{language}/{repository.split('/')[-2]}~{repository.split('/')[-1]}"
+    print(repo_path)
+
+    # getting a file total per language
+    repository_files_df:pd.DataFrame = pd.read_csv(f"{cloc_path}/{repo_path}.csv", sep=SEPARATOR)
+    repository_files_df:pd.Series = repository_files_df.groupby('language').size()
