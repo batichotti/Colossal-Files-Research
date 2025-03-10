5439ab46c037697e49cc969a1bafcfa14153ff4d
Defendi <tiagodefendidasilva@gmail.com>
2025-03-10 11:07:39 -0300

    base

---
+++ src/_09/QP1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from os import makedirs
+
+SEPARATOR = '|'
+
+# Setuup
+input_path:str = "./src/_09/input/"
+output_path:str = "./src/_09/output/"
+
+repositories_path:str = "./src/_00/input/450_Starred_Projects.csv"
+cloc_path:str = "./src/_01/output/"
+large_files_path:str = "./src/_03/output/"
+
+repositories:pd.DataFrame = pd.read_csv(repositories_path)
+
