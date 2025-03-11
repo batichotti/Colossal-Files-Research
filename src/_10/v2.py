7c173f3b9aec7d4bce7e527392d84018c349e00c
Defendi <tiagodefendidasilva@gmail.com>
2025-03-11 19:20:13 -0300

    Update v2.py

--- src/_10/v2.py
+++ src/_10/v2.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from os import makedirs, scandir, path
+from os import makedirs, scandir, path, listdir
 from concurrent.futures import ThreadPoolExecutor
 
 SEPARATOR = '|'
@@ -15,9 +15,6 @@ small_files_commits_path:str = "./src/_08/output/"
 
 repositories:pd.DataFrame = pd.read_csv(repositories_path)
 
-large_files_commits_total: list[str] = []
-small_files_commits_total: list[str] = []
-
 def process_repository(i):
     # getting repository information
     repository, language = repositories.loc[i, ['url', 'main language']]
@@ -30,51 +27,28 @@ def process_repository(i):
     hashs_large, hashs_small = [], []
     
     print(repo_path)
-    
+
+    large_files_commits_df:pd.DataFrame = pd.DataFrame()
     if path.exists(f"{large_files_commits_path}{repo_path}"):
         hashs_large = [folder.name for folder in scandir(f"{large_files_commits_path}{repo_path}") if folder.is_dir()]
-        files_large = [
-            [file.name[:-4] for file in scandir(f"{large_files_commits_path}{repo_path}/{hash}/files")]
-            for hash in hashs_large
-        ]
-        large_files_commits_df = pd.DataFrame({"hash": hashs_large, "files": files_large})
+        commits_df = pd.DataFrame()
+        for hash in hashs_large:
+            commit_df: pd.DataFrame = pd.read_csv(f"{large_files_commits_path}{repo_path}/commit.csv")
+            for file in listdir(f"{large_files_commits_path}{repo_path}/{hash}/files"):
+                file_df: pd.DataFrame = pd.read_csv(f"{large_files_commits_path}{repo_path}/{file}")
+                commits_df = pd.concat([commits_df, pd.concat([commit_df, file_df], axis=1)])
         large_files_commits_df.to_csv(f"{output_path}large_files/{repo_path}.csv", index=False)
-    
+
+    small_files_commits_df = pd.DataFrame()
     if path.exists(f"{small_files_commits_path}{repo_path}"):
         hashs_small = [folder.name for folder in scandir(f"{small_files_commits_path}{repo_path}") if folder.is_dir()]
-        files_small = [
-            [file.name[:-4] for file in scandir(f"{small_files_commits_path}{repo_path}/{hash}/files")]
-            for hash in hashs_small
-        ]
-        small_files_commits_df = pd.DataFrame({"hash": hashs_small, "files": files_small})
+        commits_df = pd.DataFrame()
+        for hash in hashs_small:
+            commit_df: pd.DataFrame = pd.read_csv(f"{small_files_commits_path}{repo_path}/commit.csv")
+            for file in listdir(f"{small_files_commits_path}{repo_path}/{hash}/files"):
+                file_df: pd.DataFrame = pd.read_csv(f"{small_files_commits_path}{repo_path}/{file}")
+                commits_df = pd.concat([commits_df, pd.concat([commit_df, file_df], axis=1)])
         small_files_commits_df.to_csv(f"{output_path}small_files/{repo_path}.csv", index=False)
-    
-    return hashs_large, hashs_small
 
 with ThreadPoolExecutor() as executor:
-    results = list(executor.map(process_repository, range(len(repositories))))
-
-for hashs_large, hashs_small in results:
-    large_files_commits_total.extend(hashs_large)
-    small_files_commits_total.extend(hashs_small)
-
-large_files_commits_total_df = pd.DataFrame(large_files_commits_total, columns=["hash"])
-large_files_commits_total_df.to_csv(f"{output_path}large_files.csv", index=False)
-
-small_files_commits_total_df = pd.DataFrame(small_files_commits_total, columns=["hash"])
-small_files_commits_total_df.to_csv(f"{output_path}small_files.csv", index=False)
-
-commits_total = list(set(large_files_commits_total + small_files_commits_total))
-commits_total_df = pd.DataFrame(commits_total, columns=["hash"])
-commits_total_df.to_csv(f"{output_path}commits_total.csv", index=False)
-
-summary = {
-    "Total de Commits Large Files": [len(large_files_commits_total)],
-    "Total de Commits Small Files": [len(small_files_commits_total)],
-    "Total de Commits": [len(commits_total)],
-}
-
-summary_df = pd.DataFrame(summary)
-summary_df.to_csv(f"{output_path}summary.csv", index=False)
-
-print("\n", summary)
+    executor.map(process_repository, range(len(repositories)))
