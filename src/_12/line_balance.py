1f2af6ff2cebe84b3ad6b94aa563e3a4a90d30be
Defendi <tiagodefendidasilva@gmail.com>
2025-03-13 21:21:03 -0300

    Update line_balance.py

--- src/_12/line_balance.py
+++ src/_12/line_balance.py
@@ -20,6 +20,12 @@ repositories:pd.DataFrame = pd.read_csv(repositories_path)
 large_files_commits: pd.DataFrame = pd.DataFrame()
 small_files_commits: pd.DataFrame = pd.DataFrame()
 
+last_language: str = repositories.loc[0, 'main language']
+large_last_language_commits: pd.DataFrame = pd.DataFrame()
+small_last_language_commits: pd.DataFrame = pd.DataFrame()
+
+
+# função ==============================================================================================================
 def calc_lines_changes(repository_commits: pd.DataFrame, type: str = "large") -> pd.DataFrame:
     # processando dados =================================================================================================
     repository_commits['Lines Balance'] = repository_commits['Lines Added'] + repository_commits['Lines Deleted']
@@ -54,37 +60,44 @@ def calc_lines_changes(repository_commits: pd.DataFrame, type: str = "large") ->
     result: pd.DataFrame = pd.DataFrame(result)
     return result
 
-# lendo csvs =======================================================================================================
-last_language: str = repositories.loc[0, ['main language']]
-large_last_language_commits: pd.DataFrame = pd.DataFrame()
-small_last_language_commits: pd.DataFrame = pd.DataFrame()
 
+# lendo csvs =======================================================================================================
 for i in range(len(repositories)):
     # getting repository information
     repository, language = repositories.loc[i, ['url', 'main language']]
 
     makedirs(f"{output_path}/per_languages", exist_ok=True)
+    makedirs(f"{output_path}/per_project", exist_ok=True)
 
     repo_path: str = f"{language}/{repository.split('/')[-2]}~{repository.split('/')[-1]}"
+
+    repository_large_files_commit: pd.DataFrame = pd.DataFrame()
+    repository_small_files_commit: pd.DataFrame = pd.DataFrame()
 
     print(repo_path)
     if (last_language != language):
         language_result: pd.DataFrame = pd.concat(calc_lines_changes(large_last_language_commits), calc_lines_changes(small_last_language_commits, "small"))
-        language_result.to_csv(f"{output_path}/per_languages/{language}.csv")
+        language_result.to_csv(f"{output_path}per_languages/{language}.csv")
+        language_result = pd.DataFrame()
 
     if path.exists(f"{large_files_commits_path}{repo_path}"):
         repository_large_files_commit = pd.read_csv(f"{large_files_commits_path}{repo_path}", sep=SEPARATOR)
         large_files_commits = pd.concat(large_files_commits, repository_large_files_commit)
-        calc_lines_changes(repository_large_files_commit).to_csv(f"{output_path}{repo_path}.csv")
         if (last_language == language):
             large_last_language_commits = pd.concat(large_last_language_commits, repository_large_files_commit)
 
     if path.exists(f"{small_files_commits_path}{repo_path}"):
         repository_small_files_commit = pd.read_csv(f"{large_files_commits_path}{repo_path}", sep=SEPARATOR)
         small_files_commits = pd.concat(small_files_commits, repository_small_files_commit)
-        calc_lines_changes(repository_large_files_commit, "small").to_csv(f"{output_path}{repo_path}.csv")
         if (last_language == language):
             small_last_language_commits = pd.concat(small_last_language_commits, repository_small_files_commit)
+
+    if (not repository_large_files_commit.empty):
+        large_project_result: pd.DataFrame = calc_lines_changes(repository_large_files_commit)
+    if (not repository_small_files_commit.empty):
+        small_project_result: pd.DataFrame = calc_lines_changes(repository_large_files_commit, "small")
+    if (not (large_project_result.empty and small_project_result)):
+        pd.concat(large_project_result, small_project_result).to_csv(f"{output_path}/per_project{repo_path}.csv")
 
 # for all =========================================================================================================================
 
