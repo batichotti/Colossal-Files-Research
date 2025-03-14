b18744d440758104f4daeddfda6f2524f02343f7
Defendi <tiagodefendidasilva@gmail.com>
2025-03-13 21:01:15 -0300

    rodando para o projeto, para linguagem, para tudo

--- src/_12/line_balance.py
+++ src/_12/line_balance.py
@@ -20,67 +20,73 @@ repositories:pd.DataFrame = pd.read_csv(repositories_path)
 large_files_commits: pd.DataFrame = pd.DataFrame()
 small_files_commits: pd.DataFrame = pd.DataFrame()
 
+def calc_lines_changes(repository_commits: pd.DataFrame, type: str = "large") -> pd.DataFrame:
+    # processando dados =================================================================================================
+    repository_commits['Lines Balance'] = repository_commits['Lines Added'] + repository_commits['Lines Deleted']
+
+    # Filtrando apenas os commits onde o tipo de mudança é "MODIFY"
+    repository_commits_modify = repository_commits[repository_commits['Change Type'] == 'MODIFY']
+
+    # linhas adicionadas
+    lines_added_avg: int = repository_commits_modify[repository_commits_modify['Lines Balance'] > 0]['Lines Balance'].mean()
+    lines_added_mean: int = repository_commits_modify[repository_commits_modify['Lines Balance'] > 0]['Lines Balance'].median()
+    lines_added_max: int = repository_commits_modify[repository_commits_modify['Lines Balance'] > 0]['Lines Balance'].max()
+
+    # linhas removidas
+    lines_deleted_avg: int = repository_commits_modify[repository_commits_modify['Lines Balance'] < 0]['Lines Balance'].mean()
+    lines_deleted_mean: int = repository_commits_modify[repository_commits_modify['Lines Balance'] < 0]['Lines Balance'].median()
+    lines_deleted_min: int = repository_commits_modify[repository_commits_modify['Lines Balance'] < 0]['Lines Balance'].min()
+
+    # contagem de zeros
+    zero_count: int = len(repository_commits_modify[repository_commits_modify['Lines Balance'] == 0])
+
+    # salvando resultado ===============================================================================================================
+    result: dict = {
+        "Type": [type],
+        "Added Average": [lines_added_avg],
+        "Added Mean": [lines_added_mean],
+        "Added Max": [lines_added_max],
+        "Deleted Average": [lines_deleted_avg],
+        "Deleted Mean": [lines_deleted_mean],
+        "Deleted Min": [lines_deleted_min],
+        "Zero Count": [zero_count]
+    }
+    result: pd.DataFrame = pd.DataFrame(result)
+    return result
 
 # lendo csvs =======================================================================================================
+last_language = repositories.loc[0, ['main language']]
+large_last_language_commits: pd.DataFrame = pd.DataFrame()
+small_last_language_commits: pd.DataFrame = pd.DataFrame()
+
 for i in range(len(repositories)):
     # getting repository information
     repository, language = repositories.loc[i, ['url', 'main language']]
 
-    makedirs(f"{output_path}", exist_ok=True)
+    makedirs(f"{output_path}/per_languages", exist_ok=True)
 
     repo_path: str = f"{language}/{repository.split('/')[-2]}~{repository.split('/')[-1]}"
 
     print(repo_path)
+    if (last_language != language):
+        language_result: pd.DataFrame = pd.concat(calc_lines_changes(large_last_language_commits), calc_lines_changes(small_last_language_commits, "small"))
+        language_result.to_csv(f"{output_path}/per_languages/{language}.csv")
 
     if path.exists(f"{large_files_commits_path}{repo_path}"):
         repository_large_files_commit = pd.read_csv(f"{large_files_commits_path}{repo_path}", sep=SEPARATOR)
         large_files_commits = pd.concat(large_files_commits, repository_large_files_commit)
+        calc_lines_changes(repository_large_files_commit).to_csv(f"{output_path}{repo_path}.csv")
+        if (last_language == language):
+            large_last_language_commits = pd.concat(large_last_language_commits, repository_large_files_commit)
+
     if path.exists(f"{small_files_commits_path}{repo_path}"):
         repository_small_files_commit = pd.read_csv(f"{large_files_commits_path}{repo_path}", sep=SEPARATOR)
         small_files_commits = pd.concat(small_files_commits, repository_small_files_commit)
+        calc_lines_changes(repository_large_files_commit, "small").to_csv(f"{output_path}{repo_path}.csv")
+        if (last_language == language):
+            small_last_language_commits = pd.concat(small_last_language_commits, repository_small_files_commit)
 
+# for all =========================================================================================================================
 
-# processando dados =================================================================================================
-large_files_commits['Lines Balance'] = large_files_commits['Lines Added'] + large_files_commits['Lines Deleted']
-small_files_commits['Lines Balance'] = small_files_commits['Lines Added'] + small_files_commits['Lines Deleted']
-
-# Filtrando apenas os commits onde o tipo de mudança é "MODIFY"
-large_files_commits_modify = large_files_commits[large_files_commits['Change Type'] == 'MODIFY']
-small_files_commits_modify = small_files_commits[small_files_commits['Change Type'] == 'MODIFY']
-
-# linhas adicionadas
-large_lines_added_avg: int = large_files_commits_modify[large_files_commits_modify['Lines Balance'] > 0]['Lines Balance'].mean()
-large_lines_added_mean: int = large_files_commits_modify[large_files_commits_modify['Lines Balance'] > 0]['Lines Balance'].median()
-large_lines_added_max: int = large_files_commits_modify[large_files_commits_modify['Lines Balance'] > 0]['Lines Balance'].max()
-
-small_lines_added_avg: int = small_files_commits_modify[small_files_commits_modify['Lines Balance'] > 0]['Lines Balance'].mean()
-small_lines_added_mean: int = small_files_commits_modify[small_files_commits_modify['Lines Balance'] > 0]['Lines Balance'].median()
-small_lines_added_max: int = small_files_commits_modify[small_files_commits_modify['Lines Balance'] > 0]['Lines Balance'].max()
-
-# linhas removidas
-large_lines_deleted_avg: int = large_files_commits_modify[large_files_commits_modify['Lines Balance'] < 0]['Lines Balance'].mean()
-large_lines_deleted_mean: int = large_files_commits_modify[large_files_commits_modify['Lines Balance'] < 0]['Lines Balance'].median()
-large_lines_deleted_min: int = large_files_commits_modify[large_files_commits_modify['Lines Balance'] < 0]['Lines Balance'].min()
-
-small_lines_deleted_avg: int = small_files_commits_modify[small_files_commits_modify['Lines Balance'] < 0]['Lines Balance'].mean()
-small_lines_deleted_mean: int = small_files_commits_modify[small_files_commits_modify['Lines Balance'] < 0]['Lines Balance'].median()
-small_lines_deleted_min: int = small_files_commits_modify[small_files_commits_modify['Lines Balance'] < 0]['Lines Balance'].min()
-
-# contagem de zeros
-large_zero_count: int = len(large_files_commits_modify[large_files_commits_modify['Lines Balance'] == 0])
-small_zero_count: int = len(small_files_commits_modify[small_files_commits_modify['Lines Balance'] == 0])
-
-
-# salvando resultado ===============================================================================================================
-result: dict = {
-    "Type": ["large", "small"],
-    "Added Average": [large_lines_added_avg, small_lines_added_avg],
-    "Added Mean": [large_lines_added_mean, small_lines_added_mean],
-    "Added Max": [large_lines_added_max, small_lines_added_max],
-    "Deleted Average": [large_lines_deleted_avg, small_lines_deleted_avg],
-    "Deleted Mean": [large_lines_deleted_mean, small_lines_deleted_mean],
-    "Deleted Min": [large_lines_deleted_min, small_lines_deleted_min],
-    "Zero Count": [large_zero_count, small_zero_count]
-}
-result: pd.DataFrame = pd.DataFrame(result)
+result : pd.DataFrame = pd.concat(calc_lines_changes(large_files_commits), calc_lines_changes(small_files_commits, "small"))
 result.to_csv(f"{output_path}/result.csv", index=False)
