1d6c63d08049a71c5f1b27f78c4f110d80570c47
Defendi <tiagodefendidasilva@gmail.com>
2025-03-21 16:56:21 -0300

    Update grew_or_decreased.py

--- src/_18/grew_or_decreased.py
+++ src/_18/grew_or_decreased.py
@@ -33,120 +33,127 @@ def grew_or_decreased(repository_commits: pd.DataFrame, change_type: str = "larg
     added_files: pd.DataFrame = repository_commits[repository_commits['Change Type'] == 'ADD'].copy()
     added_files_total: int = len(added_files)
 
-    changes = repository_commits[repository_commits['Local File PATH New'].isin(added_files['Local File PATH New'].values)].copy()
-
-    changes_large: pd.DataFrame = changes.copy()
-    if not changes_large.empty:
-        changes_large = changes_large[changes_large['File Name'].apply(lambda x: isinstance(x, str) and '.' in x)]
-        if not changes_large.empty:
-            changes_large['Extension'] = changes_large['File Name'].apply(lambda x: x.split(".")[-1]).copy()
-            changes_large = changes_large[changes_large['Extension'].isin(language_white_list_df['Extension'].values)]
-
-            changes_large = changes_large.merge(
+    if not added_files.empty:
+        added_files = added_files[added_files['File Name'].apply(lambda x: isinstance(x, str) and '.' in x)]
+        if not added_files.empty:
+            added_files['Extension'] = added_files['File Name'].apply(lambda x: x.split(".")[-1]).copy()
+            added_files = added_files[added_files['Extension'].isin(language_white_list_df['Extension'].values)]
+            added_files = added_files.merge(
                 language_white_list_df[['Extension', 'Language']],
                 on='Extension',
                 how='left'
             ).drop(columns=['Extension'])
+    added_files_filtered_total:int = len(added_files)
 
-            # Converte NLOC para numérico e remove inválidos
-            changes_large['Lines Of Code (nloc)'] = pd.to_numeric(changes_large['Lines Of Code (nloc)'], errors='coerce')
-            changes_large = changes_large.dropna(subset=['Language', 'Lines Of Code (nloc)'])
+    changes = repository_commits[repository_commits['Local File PATH New'].isin(added_files['Local File PATH New'].values)].copy()
 
-            # Filtra as linhas onde a linguagem é igual e o número de linhas de código é menor que o percentil 99
-            percentil_99 = percentil_df.set_index('language')['percentil 99']
-            changes_large = changes_large[changes_large.apply(
-                lambda x: x['Lines Of Code (nloc)'] >= percentil_99.get(x['Language'], 0), 
-                axis=1
-            )]
+    changes_large: pd.DataFrame = changes.copy()
+    if not changes_large.empty:
+        # Converte NLOC para numérico e remove inválidos
+        changes_large['Lines Of Code (nloc)'] = pd.to_numeric(changes_large['Lines Of Code (nloc)'], errors='coerce')
+        changes_large = changes_large.dropna(subset=['Language', 'Lines Of Code (nloc)'])
+
+        # Filtra as linhas onde a linguagem é igual e o número de linhas de código é menor que o percentil 99
+        percentil_99 = percentil_df.set_index('language')['percentil 99']
+        changes_large = changes_large[changes_large.apply(
+            lambda x: x['Lines Of Code (nloc)'] >= percentil_99.get(x['Language'], 0), 
+            axis=1
+        )]
 
     changes_small = pd.DataFrame()
     if not changes_large.empty:
         changes_small = changes[~changes['Local File PATH New'].isin(changes_large['Local File PATH New'].values)].copy()
         changes_large = changes[changes['Local File PATH New'].isin(changes_large['Local File PATH New'].values)].copy()
-        
-    if not changes_small.empty:
-        changes_small = changes_small[changes_small['File Name'].apply(lambda x: isinstance(x, str) and '.' in x)]
-        if not changes_small.empty:
-            changes_small['Extension'] = changes_small['File Name'].apply(lambda x: x.split(".")[-1]).copy()
-            changes_small = changes_small[changes_small['Extension'].isin(language_white_list_df['Extension'].values)]
-            changes_small = changes_small.merge(
-                language_white_list_df[['Extension', 'Language']],
-                on='Extension',
-                how='left'
-            ).drop(columns=['Extension'])
 
 
     # ANAL. ============================================================================================================
     balances_grow: list[int] = []
     balances_decreased: list[int] = []
     balances_zero: list[int] = []
+    deleted_total: int = 0
     if not changes.empty:
-        changes = changes.sort_values(by='Committer Commit Date')
-        changes_per_file = changes.groupby('Local File PATH New')
-        for _, file_changes in changes_per_file:
-            file_changes = file_changes.sort_values(by='Committer Commit Date')
-            file_changes['Lines Of Code (nloc)'] = pd.to_numeric(file_changes['Lines Of Code (nloc)'], errors='coerce')
-            file_changes = file_changes.dropna(subset=['Lines Of Code (nloc)'])
-            if not file_changes.empty:
-                first_nloc = file_changes['Lines Of Code (nloc)'].iloc[0]
-                last_nloc = file_changes['Lines Of Code (nloc)'].iloc[-1]
-                balance = last_nloc - first_nloc
-                if balance > 0:
-                    balances_grow.append(balance)
-                elif balance < 0:
-                    balances_decreased.append(balance)
-                else:
-                    balances_zero.append(balance)
+        files_deleted = changes[changes['Change Type'] == 'DELETE']
+        deleted_total = len(files_deleted)
+        changes = changes[~changes['Local File PATH New'].isin(files_deleted['Local File PATH New'].values)]
+        if not changes.empty:
+            changes = changes.sort_values(by='Committer Commit Date')
+            changes_per_file = changes.groupby('Local File PATH New')
+            for _, file_changes in changes_per_file:
+                file_changes = file_changes.sort_values(by='Committer Commit Date')
+                file_changes['Lines Of Code (nloc)'] = pd.to_numeric(file_changes['Lines Of Code (nloc)'], errors='coerce')
+                file_changes = file_changes.dropna(subset=['Lines Of Code (nloc)'])
+                if not file_changes.empty:
+                    first_nloc = file_changes['Lines Of Code (nloc)'].iloc[0]
+                    last_nloc = file_changes['Lines Of Code (nloc)'].iloc[-1]
+                    balance = last_nloc - first_nloc
+                    if balance > 0:
+                        balances_grow.append(balance)
+                    elif balance < 0:
+                        balances_decreased.append(balance)
+                    else:
+                        balances_zero.append(balance)
 
     balances_large_grow: list[int] = []
     balances_large_decreased: list[int] = []
     balances_large_zero: list[int] = []
+    deleted_large_total: int = 0
     if not changes_large.empty:
-        changes_large = changes_large.sort_values(by='Committer Commit Date')
-        changes_large_per_file = changes_large.groupby('Local File PATH New')
-        for _, file_changes in changes_large_per_file:
-            file_changes = file_changes.sort_values(by='Committer Commit Date')
-            file_changes['Lines Of Code (nloc)'] = pd.to_numeric(file_changes['Lines Of Code (nloc)'], errors='coerce')
-            file_changes = file_changes.dropna(subset=['Lines Of Code (nloc)'])
-            if not file_changes.empty:
-                first_nloc = file_changes['Lines Of Code (nloc)'].iloc[0]
-                last_nloc = file_changes['Lines Of Code (nloc)'].iloc[-1]
-                balance = last_nloc - first_nloc
-                if balance > 0:
-                    balances_large_grow.append(balance)
-                elif balance < 0:
-                    balances_large_decreased.append(balance)
-                else:
-                    balances_large_zero.append(balance)
+        files_deleted_large = changes_large[changes_large['Change Type'] == 'DELETE']
+        deleted_large_total = len(files_deleted_large)
+        changes_large = changes_large[~changes_large['Local File PATH New'].isin(files_deleted_large['Local File PATH New'].values)]
+        if not changes_large.empty:
+            changes_large = changes_large.sort_values(by='Committer Commit Date')
+            changes_large_per_file = changes_large.groupby('Local File PATH New')
+            for _, file_changes in changes_large_per_file:
+                file_changes = file_changes.sort_values(by='Committer Commit Date')
+                file_changes['Lines Of Code (nloc)'] = pd.to_numeric(file_changes['Lines Of Code (nloc)'], errors='coerce')
+                file_changes = file_changes.dropna(subset=['Lines Of Code (nloc)'])
+                if not file_changes.empty:
+                    first_nloc = file_changes['Lines Of Code (nloc)'].iloc[0]
+                    last_nloc = file_changes['Lines Of Code (nloc)'].iloc[-1]
+                    balance = last_nloc - first_nloc
+                    if balance > 0:
+                        balances_large_grow.append(balance)
+                    elif balance < 0:
+                        balances_large_decreased.append(balance)
+                    else:
+                        balances_large_zero.append(balance)
 
     balances_small_grow: list[int] = []
     balances_small_decreased: list[int] = []
     balances_small_zero: list[int] = []
+    deleted_small_total: int = 0
     if not changes_small.empty:
-        changes_small = changes_small.sort_values(by='Committer Commit Date')
-        changes_small_per_file = changes_small.groupby('Local File PATH New')
-        for _, file_changes in changes_small_per_file:
-            file_changes = file_changes.sort_values(by='Committer Commit Date')
-            file_changes['Lines Of Code (nloc)'] = pd.to_numeric(file_changes['Lines Of Code (nloc)'], errors='coerce')
-            file_changes = file_changes.dropna(subset=['Lines Of Code (nloc)'])
-            if not file_changes.empty:
-                first_nloc = file_changes['Lines Of Code (nloc)'].iloc[0]
-                last_nloc = file_changes['Lines Of Code (nloc)'].iloc[-1]
-                balance = last_nloc - first_nloc
-                if balance > 0:
-                    balances_small_grow.append(balance)
-                elif balance < 0:
-                    balances_small_decreased.append(balance)
-                else:
-                    balances_small_zero.append(balance)
+        files_deleted_small = changes_small[changes_small['Change Type'] == 'DELETE']
+        deleted_small_total = len(files_deleted_small)
+        changes_small = changes_small[~changes_small['Local File PATH New'].isin(files_deleted_small['Local File PATH New'].values)]
+        if not changes_small.empty:
+            changes_small = changes_small.sort_values(by='Committer Commit Date')
+            changes_small_per_file = changes_small.groupby('Local File PATH New')
+            for _, file_changes in changes_small_per_file:
+                file_changes = file_changes.sort_values(by='Committer Commit Date')
+                file_changes['Lines Of Code (nloc)'] = pd.to_numeric(file_changes['Lines Of Code (nloc)'], errors='coerce')
+                file_changes = file_changes.dropna(subset=['Lines Of Code (nloc)'])
+                if not file_changes.empty:
+                    first_nloc = file_changes['Lines Of Code (nloc)'].iloc[0]
+                    last_nloc = file_changes['Lines Of Code (nloc)'].iloc[-1]
+                    balance = last_nloc - first_nloc
+                    if balance > 0:
+                        balances_small_grow.append(balance)
+                    elif balance < 0:
+                        balances_small_decreased.append(balance)
+                    else:
+                        balances_small_zero.append(balance)
     
     # Result =================================================================================================================
     result: dict = {
         "Type": [change_type],
         "#Files": [added_files_total],
-        "%% Grow": [len(balances_grow)/added_files_total if added_files_total > 0 else 0],
-        "%% Decrease": [len(balances_decreased)/added_files_total if added_files_total > 0 else 0],
-        "%% Zero": [len(balances_zero)/added_files_total if added_files_total > 0 else 0],
+        "#Files Filtered by Language": [added_files_filtered_total],
+        "%% Grow": [(len(balances_grow)/added_files_filtered_total)*100 if added_files_filtered_total > 0 else 0],
+        "%% Decrease": [(len(balances_decreased)/added_files_filtered_total)*100 if added_files_filtered_total > 0 else 0],
+        "%% Zero": [(len(balances_zero)/added_files_filtered_total)*100 if added_files_filtered_total > 0 else 0],
+        "%% Deleted": [(deleted_total/added_files_filtered_total)*100 if added_files_filtered_total > 0 else 0],
 
         "Total File Grow": [len(balances_grow)],
         "Grow Average": [np.mean(balances_grow) if balances_grow else 0],
@@ -155,8 +162,9 @@ def grew_or_decreased(repository_commits: pd.DataFrame, change_type: str = "larg
         "Total Decreased": [len(balances_decreased)],
         "Decreased Average": [np.mean(balances_decreased) if balances_decreased else 0],
         "Decreased Median": [np.median(balances_decreased) if balances_decreased else 0],
-        "Decreased Max": [np.max(balances_decreased) if balances_decreased else 0],
+        "Decreased Max": [np.min(balances_decreased) if balances_decreased else 0],
         "Total Zero": [len(balances_zero)],
+        "Total Deleted": [deleted_total],
 
         "Total Large Grow": [len(balances_large_grow)],
         "Grow Large Average": [np.mean(balances_large_grow) if balances_large_grow else 0],
@@ -165,8 +173,9 @@ def grew_or_decreased(repository_commits: pd.DataFrame, change_type: str = "larg
         "Total Large Decreased": [len(balances_large_decreased)],
         "Decreased Large Average": [np.mean(balances_large_decreased) if balances_large_decreased else 0],
         "Decreased Large Median": [np.median(balances_large_decreased) if balances_large_decreased else 0],
-        "Decreased Large Max": [np.max(balances_large_decreased) if balances_large_decreased else 0],
+        "Decreased Large Max": [np.min(balances_large_decreased) if balances_large_decreased else 0],
         "Total Large Zero": [len(balances_large_zero)],
+        "Total Large Deleted": [deleted_large_total],
 
         "Total Small Grow": [len(balances_small_grow)],
         "Grow Small Average": [np.mean(balances_small_grow) if balances_small_grow else 0],
@@ -175,8 +184,9 @@ def grew_or_decreased(repository_commits: pd.DataFrame, change_type: str = "larg
         "Total Small Decreased": [len(balances_small_decreased)],
         "Decreased Small Average": [np.mean(balances_small_decreased) if balances_small_decreased else 0],
         "Decreased Small Median": [np.median(balances_small_decreased) if balances_small_decreased else 0],
-        "Decreased Small Max": [np.max(balances_small_decreased) if balances_small_decreased else 0],
-        "Total Small Zero": [len(balances_small_zero)]
+        "Decreased Small Max": [np.min(balances_small_decreased) if balances_small_decreased else 0],
+        "Total Small Zero": [len(balances_small_zero)],
+        "Total Small Deleted": [deleted_small_total],
     }
     return pd.DataFrame(result)
 
