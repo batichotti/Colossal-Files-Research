bf5f2bb7196d9ef81e2652919b7dc2e2d0e32eb5
Defendi <tiagodefendidasilva@gmail.com>
2025-04-22 20:53:33 -0300

    espero q va

--- test/modify/modify.py
+++ test/modify/modify.py
@@ -13,7 +13,7 @@ input_path: str = "./test/modify/input/"
 output_path: str = "./test/modify/output/"
 
 repositories_path: str = "./src/_00/input/450_Starred_Projects.csv"
-modify_path: str = "./test/modify/output/per_projct"
+modify_path: str = "./test/modify/output/per_projct/"
 language_white_list_path: str = "./src/_12/input/white_list.csv"
 percentil_path: str = "./src/_02/output/percentis_by_language_filtered.csv"
 large_files_commits_path: str = "./src/_10/output/large_files/"
@@ -152,17 +152,104 @@ def calc_metrics(df:pd.DataFrame) -> pd.DataFrame:
     groups = df.groupby("Dataset")
     dataset_large = groups.get_group("large") if "large" in groups.groups else pd.DataFrame()
     dataset_small = groups.get_group("small") if "small" in groups.groups else pd.DataFrame()
-    
-    large_commits 
-    large_only_added
-    large_life_time
-    large_modifications_interal
-    large_deleted_total
+    large_commits = 0
+    large_only_added = 0
+    large_amount_mean = 0
+    large_amount_median = 0
+    large_life_time_mean = 0
+    large_life_time_median = 0
+    large_interval_mean = 0
+    large_interval_median = 0
+    large_deleted_total = 0
+    large_deleted_amount_mean = 0
+    large_deleted_amount_median = 0
+    large_deleted_life_time_mean = 0
+    large_deleted_life_time_median = 0
+    large_deleted_interval_mean = 0
+    large_deleted_interval_median = 0
     if not dataset_large.empty:
-        ...
+        large_commits = dataset_large["Modifications' Amount"].sum()
+        large_only_added = len(dataset_large[dataset_large["Only Added?"] == True])
+        large_amount_mean = dataset_large["Modifications' Amount"].mean()
+        large_amount_median = dataset_large["Modifications' Amount"].median()
+        large_life_time_mean = dataset_large["Life Time (day)"].mean()
+        large_life_time_median = dataset_large["Life Time (day)"].median()
+        large_interval_mean = dataset_large["Modifications' Interval (day)"].mean()
+        large_interval_median = dataset_large["Modifications' Interval (day)"].median()
+        large_deleted_total = len(dataset_large[dataset_large["Deleted?"] == True])
+        large_deleted_amount_mean = dataset_large[dataset_large["Deleted?"] == True]["Modifications' Amount"].mean()
+        large_deleted_amount_median = dataset_large[dataset_large["Deleted?"] == True]["Modifications' Amount"].median()
+        large_deleted_life_time_mean = dataset_large[dataset_large["Deleted?"] == True]["Life Time (day)"].mean()
+        large_deleted_life_time_median = dataset_large[dataset_large["Deleted?"] == True]["Life Time (day)"].median()
+        large_deleted_interval_mean = dataset_large[dataset_large["Deleted?"] == True]["Modifications' Interval (day)"].mean()
+        large_deleted_interval_median = dataset_large[dataset_large["Deleted?"] == True]["Modifications' Interval (day)"].median()
+
+    small_commits = 0
+    small_only_added = 0
+    small_amount_mean = 0
+    small_amount_median = 0
+    small_life_time_mean = 0
+    small_life_time_median = 0
+    small_interval_mean = 0
+    small_interval_median = 0
+    small_deleted_total = 0
+    small_deleted_amount_mean = 0
+    small_deleted_amount_median = 0
+    small_deleted_life_time_mean = 0
+    small_deleted_life_time_median = 0
+    small_deleted_interval_mean = 0
+    small_deleted_interval_median = 0
+    if not dataset_small.empty:
+        small_commits = dataset_small["Modifications' Amount"].sum()
+        small_only_added = len(dataset_small[dataset_small["Only Added?"] == True])
+        small_amount_mean = dataset_small["Modifications' Amount"].mean()
+        small_amount_median = dataset_small["Modifications' Amount"].median()
+        small_life_time_mean = dataset_small["Life Time (day)"].mean()
+        small_life_time_median = dataset_small["Life Time (day)"].median()
+        small_interval_mean = dataset_small["Modifications' Interval (day)"].mean()
+        small_interval_median = dataset_small["Modifications' Interval (day)"].median()
+        small_deleted_total = len(dataset_small[dataset_small["Deleted?"] == True])
+        small_deleted_amount_mean = dataset_small[dataset_small["Deleted?"] == True]["Modifications' Amount"].mean()
+        small_deleted_amount_median = dataset_small[dataset_small["Deleted?"] == True]["Modifications' Amount"].median()
+        small_deleted_life_time_mean = dataset_small[dataset_small["Deleted?"] == True]["Life Time (day)"].mean()
+        small_deleted_life_time_median = dataset_small[dataset_small["Deleted?"] == True]["Life Time (day)"].median()
+        small_deleted_interval_mean = dataset_small[dataset_small["Deleted?"] == True]["Modifications' Interval (day)"].mean()
+        small_deleted_interval_median = dataset_small[dataset_small["Deleted?"] == True]["Modifications' Interval (day)"].median()
 
     result = {
+        "Large Commits": large_commits,
+        "Large Only Added": large_only_added,
+        "Large Amount Mean": large_amount_mean,
+        "Large Amount Median": large_amount_median,
+        "Large Life Time Mean": large_life_time_mean,
+        "Large Life Time Median": large_life_time_median,
+        "Large Interval Mean": large_interval_mean,
+        "Large Interval Median": large_interval_median,
+        "Large Deleted Total": large_deleted_total,
+        "Large Deleted Amount Mean": large_deleted_amount_mean,
+        "Large Deleted Amount Median": large_deleted_amount_median,
+        "Large Deleted Life Time Mean": large_deleted_life_time_mean,
+        "Large Deleted Life Time Median": large_deleted_life_time_median,
+        "Large Deleted Interval Mean": large_deleted_interval_mean,
+        "Large Deleted Interval Median": large_deleted_interval_median,
+
+        "Small Commits": small_commits,
+        "Small Only Added": small_only_added,
+        "Small Amount Mean": small_amount_mean,
+        "Small Amount Median": small_amount_median,
+        "Small Life Time Mean": small_life_time_mean,
+        "Small Life Time Median": small_life_time_median,
+        "Small Interval Mean": small_interval_mean,
+        "Small Interval Median": small_interval_median,
+        "Small Deleted Total": small_deleted_total,
+        "Small Deleted Amount Mean": small_deleted_amount_mean,
+        "Small Deleted Amount Median": small_deleted_amount_median,
+        "Small Deleted Life Time Mean": small_deleted_life_time_mean,
+        "Small Deleted Life Time Median": small_deleted_life_time_median,
+        "Small Deleted Interval Mean": small_deleted_interval_mean,
+        "Small Deleted Interval Median": small_deleted_interval_median,
     }
+    return pd.DataFrame(result)
 
 # Processamento principal =====================================================================================
 
@@ -199,9 +286,11 @@ for i, row in repositories.iterrows():
 
     if project_results:
         pd.concat(project_results).to_csv(f"{output_path}/per_project/{repo_path}.csv", index=False)
-        
-        
+
+
 # ================================================================================================================
+all_df = pd.DataFrame()
+
 current_language: str = None
 current_df: pd.DataFrame = pd.DataFrame()
 
@@ -215,50 +304,29 @@ for i, row in repositories.iterrows():
     print(repo_path)
 
     # Cria diretórios necessários
-    makedirs(f"{output_path}/general/{language}", exist_ok=True)
-    
+    makedirs(f"{output_path}general/per_project/{language}", exist_ok=True)
+    makedirs(f"{output_path}general/per_language/", exist_ok=True)
+
     # Atualiza acumuladores de linguagem quando muda
     if current_language and (language != current_language):
-        #roda para lingagem
+        calc_metrics(current_df).to_csv(f"{output_path}general/per_language/{current_language}.csv", index=False)
+        all_df = pd.concat([all_df, current_df])
         current_df = pd.DataFrame()
-    
-    current_language = language
-    
-    # Processa arquivos grandes
-    large_df: pd.DataFrame = pd.DataFrame()
-    large_path = f"{large_files_commits_path}{repo_path}.csv"
-    if path.exists(large_path):
-        large_df: pd.DataFrame = pd.read_csv(large_path, sep=SEPARATOR)
-        current_large = pd.concat([current_large, large_df])
-        large_files_commits = pd.concat([large_files_commits, large_df])
-    
-    # Processa arquivos pequenos
-    small_path = f"{small_files_commits_path}{repo_path}.csv"
-    small_df: pd.DataFrame = pd.DataFrame()
-    if path.exists(small_path):
-        small_df: pd.DataFrame = pd.read_csv(small_path, sep=SEPARATOR)
-        current_small = pd.concat([current_small, small_df])
-        small_files_commits = pd.concat([small_files_commits, small_df])
-    
-    project_results: list[pd.DataFrame] = []
-    if not large_df.empty:
-        project_results.append(frequency_by_lifetime(large_df))
-    if not small_df.empty:
-        project_results.append(frequency_by_lifetime(small_df, 'small'))
 
-    if project_results:
-        pd.concat(project_results).to_csv(f"{output_path}/per_project/{repo_path}.csv", index=False)
+    current_language = language
+
+    # Processa por projeto
+    df = pd.DataFrame()
+    if path.exists(f"{modify_path}{repo_path}.csv"):
+        df = pd.read_csv(f"{modify_path}{repo_path}.csv")
+        calc_metrics(df).to_csv(f"{output_path}general/per_project/{repo_path}.csv", index=False)
+        current_df = pd.concat([current_df, df])
 
 # Processa última linguagem
-if not current_large.empty or not current_small.empty:
-    ...
+if not current_df.empty:
+    calc_metrics(current_df).to_csv(f"{output_path}general/per_language/{current_language}.csv", index=False)
+    all_df = pd.concat([all_df, current_df])
 
 # Resultado global ============================================================================================
-final_results: list[pd.DataFrame] = []
-if not large_files_commits.empty:
-    final_results.append(frequency_by_lifetime(large_files_commits))
-if not small_files_commits.empty:
-    final_results.append(frequency_by_lifetime(small_files_commits, 'small'))
-
-if final_results:
-    pd.concat(final_results).to_csv(f"{output_path}/global_results.csv", index=False)
+if not all_df.empty:
+    calc_metrics(all_df).to_csv(f"{output_path}general/global_results.csv", index=False)
